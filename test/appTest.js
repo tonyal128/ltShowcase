75bc2178da4cb267691a07b2310fab426643e990
tonyal128 <anthony.r.larson@gmail.com>
2018-05-05 22:36:54 -0500

    updating isNumber test to include more defined test cases

--- test/appTest.js
+++ test/appTest.js
@@ -5,8 +5,11 @@ const isJson = require('../app.js').isJson;
 
 describe('App', function(){
     describe('isNumber', function(){
-        it('isNumber should return boolean of true', function(){
-            assert.equal(isNumber(true), true);
+        it('isNumber should return boolean of true if passed a number', function(){
+            assert.equal(isNumber(1), true);
+        });
+        it('isNumber should return boolean of false if passed a NaN', function(){
+            assert.equal(isNumber("string"), false);
         });
         it('isNumber should be a function', function(){
             assert.isFunction(isNumber);
