f4edbc62a71536a4bd7691f03d50fa1da34f28eb
tonyal128 <anthony.r.larson@gmail.com>
2018-05-06 20:31:17 -0500

    adding unit test for getPhotos

--- test/appTest.js
+++ test/appTest.js
@@ -1,4 +1,6 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
+const itParam = require('mocha-param');
 const isNumber = require('../app.js').isNumber;
 const isJson = require('../app.js').isJson;
 
@@ -14,13 +16,27 @@ describe('App', function(){
         it('isNumber should be a function', function(){
             assert.isFunction(isNumber);
         })
+        itParam('argument should be a number',[1], function(value){
+            expect(value).to.be.a('number');
+        });
+        itParam('argument should be greater than 0',[1], function(value){
+            expect(value).to.be.greaterThan(0);
+        });
     });
     describe('isJson', function(){
-        it('isJson should return boolean of true', function(){
-            assert.equal(isJson(true), true);
+        it('isJson should return boolean of false if not passed JSON', function(){
+            assert.equal(isJson('not json'), false);
         });
         it('isJson should be a function', function(){
             assert.isFunction(isJson);
         })
+    });
+    describe('getPhotos', function(){
+        itParam('argument should be a number',[1], function(value){
+            expect(value).to.be.a('number');
+        });
+        itParam('argument should be greater than 0',[1], function(value){
+            expect(value).to.be.greaterThan(0);
+        });
     });
 });
